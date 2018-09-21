4c8d2bfa28b90f0da2bab900698711f1cd1c945e
Teddy Katz <teddy.katz@gmail.com>
2018-09-20 20:38:56 -0400

    Add definitions/example for a well-behaved network policy

--- NetworkConfigurations.v
+++ NetworkConfigurations.v
@@ -1,24 +1,89 @@
-(* TODO: add bounds for port in [0, 65535) *)
-Definition port := nat.
+Section Node.
+  Variable Node : Set.
+  Record flow := {
+    Src : Node;
+    Dest : Node
+  }.
 
-(* TODO: add bounds for IP components in [0, 256) *)
-(* TODO: support IPv6 addresses? *)
-Definition ipAddress : Set := (nat * nat * nat * nat).
+  Definition network_topology := Node -> Node -> Prop.
 
-(* TODO: support more interesting protocol info here *)
-Definition protocol := unit.
+  Definition network_policy := flow -> bool.
 
-Record flow := {
-  SrcIp : ipAddress;
-  SrcPort : port;
-  DestIp : ipAddress;
-  DestPort : port;
-  Protocol : protocol
-}.
+  Definition next_node := Node -> Node -> Node -> Prop.
 
-Definition network_topology (Router : Set) := Router -> Router -> bool.
+  Fixpoint is_next_node_path (next : next_node) path src dest :=
+    match path with
+    | nil => src = dest
+    | cons hop_target cdr =>
+        next src dest hop_target /\
+        is_next_node_path next cdr hop_target dest
+    end.
 
-Definition network_policy R := network_topology R -> flow -> bool.
+  Record next_node_valid (topology : network_topology) (policy : network_policy) (next : next_node) := {
+    all_hops_in_topology : forall src dest hop_target,
+      next src dest hop_target -> topology src hop_target;
 
-Definition everything_allowed_policy : network_policy ipAddress :=
-  (fun _ _ => true).
+    path_exists_for_valid_flows : forall flow,
+      policy flow = true
+        -> exists path, is_next_node_path next path flow.(Src) flow.(Dest);
+
+    no_dead_ends : forall src dest hop_target,
+      next src dest hop_target
+        -> exists path, is_next_node_path next path hop_target dest;
+
+    all_paths_acyclic : forall node path,
+      is_next_node_path next path node node -> path = nil
+  }.
+End Node.
+
+Require Import ZArith.
+Section NatNetworkExample.
+  Local Definition nat_topology src dest := src - dest < 10.
+  Local Definition nat_policy (f : flow nat) := Dest nat f <=? Src nat f.
+  Local Inductive nat_next_node : next_node nat :=
+  | MinusOne : forall next_src dest,
+    next_src >= dest -> nat_next_node (S next_src) dest next_src
+  | MinusTwo : forall next_src dest,
+    next_src >= dest -> nat_next_node (S (S next_src)) dest next_src.
+
+  Local Lemma reducing_exists : forall src dest,
+    src >= dest
+      -> exists path, is_next_node_path nat nat_next_node path src dest.
+  Proof.
+    intros.
+    induction src.
+    assert (dest = 0) by omega; subst.
+    exists nil.
+    reflexivity.
+    assert (dest <= src \/ dest = S src) by omega.
+    case H0; intros.
+    assert (H' := H1).
+    apply IHsrc in H1.
+    destruct H1.
+    exists (cons src x).
+    simpl.
+    apply conj; try assumption.
+    constructor.
+    omega.
+    subst.
+    exists nil.
+    reflexivity.
+  Qed.
+
+  Local Theorem nat_next_node_valid : next_node_valid nat nat_topology nat_policy nat_next_node.
+  Proof.
+    constructor; intros.
+    unfold nat_topology.
+    inversion H; omega.
+    unfold nat_policy in H.
+    apply Nat.leb_le in H.
+    apply reducing_exists.
+    omega.
+    inversion H; apply reducing_exists; assumption.
+    destruct path.
+    reflexivity.
+    simpl in H.
+    destruct H.
+    inversion H; omega.
+  Qed.
+End NatNetworkExample.
